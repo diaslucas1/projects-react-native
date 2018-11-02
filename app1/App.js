964cf1d24b316271a681c5d3d112ee1baaa3acbe
Lucas Dias <lucasdiasrodrigues@hotmail.com>
2018-11-02 19:24:20 -0300

    First commit

---
+++ app1/App.js
@@ -0,0 +1,14 @@
+var React = require('react');
+var Text = require('react-native').Text;
+var AppRegistry = require('react-native').AppRegistry;
+
+const App = () => {
+
+  return (
+    <Text>Meu primeiro APP</Text>
+  );
+};
+
+export default App;
+
+AppRegistry.registerComponent('app1', () => App);
