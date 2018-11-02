d5c9f6b413c4867d56627398c41dfa6b87c4846d
Lucas Dias <lucasdiasrodrigues@hotmail.com>
2018-11-02 19:51:41 -0300

    Random number generator completed

--- app1/App.js
+++ app1/App.js
@@ -1,11 +1,23 @@
-var React = require('react');
-var Text = require('react-native').Text;
-var AppRegistry = require('react-native').AppRegistry;
+import React from 'react';
+import { Text, View, Button, AppRegistry } from 'react-native';
+
+const geraNumeroAleatorio = () => {
+  var numero_aleatorio = Math.random();
+
+  numero_aleatorio = Math.floor(numero_aleatorio * 10);
+  alert(numero_aleatorio);
+}
 
 const App = () => {
 
   return (
-    <Text>Meu primeiro APP</Text>
+    <View>
+      <Text>Gerador de números randômicos!!</Text>
+      <Button 
+        title="Gerar um número randômico"
+        onPress={geraNumeroAleatorio}
+      />
+    </View>
   );
 };
 
