5ee0ed06d024d7ff3b47c5391e2517fc22b18f62
Lucas Dias <lucasdiasrodrigues@hotmail.com>
2018-11-03 12:00:46 -0300

    Mudança no layout

--- app2/App.js
+++ app2/App.js
@@ -1,18 +1,45 @@
 //Import
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { Text, AppRegistry, View, Image, TouchableOpacity } from 'react-native';
 
 //Formatações
 const Estilos = {
-  estiloTexto: {
-    fontSize: 50
+  principal: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  botao: {
+    backgroundColor: '#538530',
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    marginTop: 20
+  },
+  textoBotao: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold'
   }
+};
+
+const botaoPressionado = () => {
+
 };
 
 //Criar o componente
 const App = () => {
+
+  const { principal, botao, textoBotao } = Estilos;
+
   return (
-    <Text style={Estilos.estiloTexto}>Frases do dia</Text>
+    <View style={principal}>
+      
+      <Image source={require('./imgs/logo.png')}/>
+      <TouchableOpacity style={botao}>
+        <Text style={textoBotao}>Nova frase</Text>
+      </TouchableOpacity>
+
+    </View>
   );
 };
 
