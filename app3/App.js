758eb635e3750ab1624bda867dbbdb183fd630ff
Lucas Dias <lucasdiasrodrigues@hotmail.com>
2018-11-20 22:50:41 -0200

    Definindo resultado

--- app3/App.js
+++ app3/App.js
@@ -14,19 +14,75 @@ export default class app3 extends Component {
   constructor(props){
     super(props);
 
-    this.state = { escolhaUsuario : '' }
+    this.state = { escolhaUsuario : '', 
+                   escolhaComputador : '',
+                   resultado : '' 
+                 }
   }
 
   jokenpo(escolhaUsuario){
-    this.setState({ escolhaUsuario : escolhaUsuario }); //alteração de uma variável de estado - this.setState({ variável : valor })
+
+    //gera número aleatório (0, 1, 2)
+    var numAleatorio = Math.floor(Math.random() * 3);
+
+    var escolhaComputador = '';
+
+    switch(numAleatorio){
+      case 0: escolhaComputador = 'pedra'; break;
+      case 1: escolhaComputador = 'papel'; break;
+      case 2: escolhaComputador = 'tesoura'; break;
+    }
+
+    var resultado = '';
+
+    if(escolhaComputador == 'pedra'){
+      if(escolhaUsuario == 'pedra'){
+        resultado = 'Empate';
+      }
+
+      if(escolhaUsuario == 'papel'){
+        resultado = 'Usuário ganhou';
+      } else{
+        resultado = 'Computador ganhou';
+      }
+    }
+
+    if(escolhaComputador == 'papel'){
+      if(escolhaUsuario == 'papel'){
+        resultado = 'Empate';
+      }
+
+      if(escolhaUsuario == 'tesoura'){
+        resultado = 'Usuário ganhou';
+      } else{
+        resultado = 'Computador ganhou';
+      }
+    }
+
+    if(escolhaComputador == 'tesoura'){
+      if(escolhaUsuario == 'tesoura'){
+        resultado = 'Empate';
+      }
+
+      if(escolhaUsuario == 'pedra'){
+        resultado = 'Usuário ganhou';
+      } else{
+        resultado = 'Computador ganhou';
+      }
+    }
+
+    this.setState({ escolhaUsuario : escolhaUsuario, 
+                    escolhaComputador : escolhaComputador,
+                    resultado : resultado
+                  }); //alteração de uma variável de estado - this.setState({ variável : valor })
   }
   
   render(){
     return (
       <View>
-        <Text>Escolha do computador</Text>
+        <Text>Escolha do computador: {this.state.escolhaComputador}</Text>
         <Text>Escolha do usuário: {this.state.escolhaUsuario}</Text>
-        <Text>Resultado</Text>
+        <Text>Resultado: {this.state.resultado}</Text>
         <Button title="pedra" onPress={ () => {this.jokenpo('pedra')}}/> 
         <Button title="papel" onPress={ () => {this.jokenpo('papel')}}/>
         <Button title="tesoura" onPress={ () => {this.jokenpo('tesoura')}}/>
